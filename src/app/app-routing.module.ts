53e30f3965e04dc17d567bda09f825a370dc81da
K S Anoop <anoop2014.aks@gmail.com>
2023-09-10 23:53:48 +0530

    initial-commit-heading-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'customers', component: CustomersComponent },
+  {path: 'orders', component: OrdersComponent },
+  {path: 'analytics', component: AnalyticsComponent },
+  {path: '**', redirectTo: '/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CustomersComponent,
+    OrdersComponent,
+    AnalyticsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
